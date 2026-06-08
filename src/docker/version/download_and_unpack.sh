3c3e6b9691dec2f5ad1ff3058bf219efbcea59ae
Christoph <debuas1999@gmail.com>
2026-06-08 11:31:01 +0200

    Update download_and_unpack.sh

--- src/docker/version/download_and_unpack.sh
+++ src/docker/version/download_and_unpack.sh
@@ -25,8 +25,8 @@ fi
 raw_download_list=$(curl -s "${baseurl}?raw")
 version=$(echo "$1" | tr -d '"')
 # Filter and select the right version with the non-Java8 version
-selected_download=$(echo "$raw_download_list" | grep "GT_New_Horizons_${version}_Server_Java_[0-9]*-[0-9]*.zip" | tail -n 1)
-
+#selected_download=$(echo "$raw_download_list" | grep "GT_New_Horizons_${version}_Server_Java_[0-9]*-[0-9]*.zip" | tail -n 1)
+selected_download=$(echo "${baseurl}GT_New_Horizons_${version}_Server_Java_17-25.zip" | tail -n 1)
 if [ -z "$selected_download" ]; then
     echo "No matching version found for $1."
     exit 1
